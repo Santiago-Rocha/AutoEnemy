ac98e41128da55b766f819046c7c8f4dc554c9db
Santiago-Rocha <santiago.rocha@tulpep.com>
2019-11-26 01:20:06 -0500

    add script

--- suspect.py
+++ suspect.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 import time
+import os
 
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 TOPIC = "srd98"
 
+driverPath = os.path.abspath(PROJECT_ROOT+"/driver/chromedriver.exe")
 
-driverChrome = webdriver.Chrome("C:/Users/pgr20192/Documents/AutoEnemy/driver/chromedriver.exe")
+
+driverChrome = webdriver.Chrome(driverPath)
+
 
 driverChrome.set_page_load_timeout(10)
 driverChrome.get("https://www.omegle.com/")
@@ -23,14 +29,15 @@ while (logsNumber == len(driverChrome.find_elements_by_class_name("logitem"))):
 
 while(True):
     logsNumber = len(driverChrome.find_elements_by_class_name("logitem"))
-    while(logsNumber == len(driverChrome.find_elements_by_class_name("logitem"))):
-        #if(driverChrome.find_elements_by_class_name("logitem")[-1].get)
-        print(logsNumber,len(driverChrome.find_elements_by_class_name("logitem")) )
-        #print("Esperando Respuesta")
+    while(logsNumber == len(driverChrome.find_elements_by_class_name("logitem")) or driverChrome.find_elements_by_class_name("logitem")[-1].text == "Stranger is typing..."):
+        #if(driverChrome.find_elements_by_class_name("logitem")[-1].text == "Stranger is typing..."): continue
+        #print(logsNumber,len(driverChrome.find_elements_by_class_name("logitem")) )
+        #print(driverChrome.find_elements_by_class_name("logitem")[-1])
+        #print(driverChrome.find_elements_by_class_name("logitem")[-1].text)
+        time.sleep(1)
+        
     driverChrome.find_element_by_class_name("chatmsg ").send_keys("Hola")
     driverChrome.find_element_by_class_name("sendbtn").click()
     print(logsNumber,len(driverChrome.find_elements_by_class_name("logitem")) )
-    time.sleep(1)
-
-
+    time.sleep(3)
 
